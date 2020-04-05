391c48c0e8425e087348da4b606555b88b70ef5b
Cris M <cristian.montenegro.p@gmail.com>
2020-04-05 04:01:05 -0700

    localStorage retrieval and saving functions were sucessfully created and tested

--- script.js
+++ script.js
@@ -10,28 +10,52 @@ currentDate();
 var now = new Date();
 var currentHour = now.getHours();
 // var currentHour = 12;
-console.log(currentHour);
-console.log(now);
+// console.log(currentHour);
+// console.log(now);
 
-$(".textarea").each(function (){
-
+$(".textarea").each(function () {
 
     if ($(this).attr("block-hour") == currentHour) {
-        console.log("==")
+        // console.log("==")
         $(this).addClass("present");
     }
 
     if ($(this).attr("block-hour") > currentHour) {
-        console.log("<")
+        // console.log("<")
         $(this).addClass("future");
     }
 
     if ($(this).attr("block-hour") < currentHour) {
-        console.log(">")
+        // console.log(">")
         $(this).addClass("past");
     }
 
 });
 
+var appointmentsBlock = {};
 
-// Function that captures the input on each time-block and saves it in the localStorage
+// retrieving of previously stored information 
+appointmentsBlock = JSON.parse(localStorage.getItem("appointmentsBlock"));
+console.log(appointmentsBlock);
+$("input[type=text]").each(function () {
+
+    $(this).val(appointmentsBlock[$(this).attr("block-hour")]);
+});
+
+// Function that captures the input on each time-block and saves it in the
+//  localStorage when any button is pressed
+$("button").on("click", function () {
+
+    $("input[type=text]").each(function () {
+
+        appointmentsBlock[$(this).attr("block-hour")] = $(this).val();
+    });
+
+    localStorage.setItem("appointmentsBlock", JSON.stringify(appointmentsBlock));
+    // console.log(localStorage);
+    // console.log(appointmentsBlock);
+});
+
+
+
+

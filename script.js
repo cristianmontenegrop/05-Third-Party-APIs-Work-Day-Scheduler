6442f38ea5ec9be9f3f28a75fff494c10dc4d911
Cris M <cristian.montenegro.p@gmail.com>
2020-04-05 07:14:14 -0700

    Fixing of the bug finalized

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
+// this if statement fixes a bug related to the variable appointmentBlock showing as null
+
+// localStorage.setItem("appointmentsBlock", JSON.stringify(appointmentsBlock));
+
+
+
 // Current date Function
 function currentDate() {
     var momentDate = moment().format('LLLL');
     $("#currentDay").text(momentDate);
-}
+};
 currentDate();
 
 // Function that  changes the block colors
@@ -18,27 +24,40 @@ $(".textarea").each(function () {
     if ($(this).attr("block-hour") == currentHour) {
         // console.log("==")
         $(this).addClass("present");
-    }
+    };
 
     if ($(this).attr("block-hour") > currentHour) {
         // console.log("<")
         $(this).addClass("future");
-    }
+    };
 
     if ($(this).attr("block-hour") < currentHour) {
         // console.log(">")
         $(this).addClass("past");
-    }
+    };
 
 });
 
+// retrieving of previously stored information  
 var appointmentsBlock = {};
 
-// retrieving of previously stored information 
-appointmentsBlock = JSON.parse(localStorage.getItem("appointmentsBlock"));
-console.log(appointmentsBlock);
-$("input[type=text]").each(function () {
+// $().ready(function () {
 
+// console.log(appointmentsBlock);
+
+var storageLocal = JSON.parse(localStorage.getItem("appointmentsBlock"));
+if (storageLocal === null) {
+  
+    console.log("It's Null!")
+} else { 
+     appointmentsBlock = JSON.parse(localStorage.getItem("appointmentsBlock"));
+     console.log("It's NOT Null!, run getItem")
+};
+
+// console.log(storageLocal);
+// console.log(appointmentsBlock);
+
+$("input[type=text]").each(function () {
     $(this).val(appointmentsBlock[$(this).attr("block-hour")]);
 });
 
@@ -47,15 +66,18 @@ $("input[type=text]").each(function () {
 $("button").on("click", function () {
 
     $("input[type=text]").each(function () {
+        // console.log(this);
+        (appointmentsBlock)[$(this).attr("block-hour")] = $(this).val();
+        // console.log(appointmentsBlock);
+        // console.log($(this).attr("block-hour"));
+        // console.log($(this).val());
 
-        appointmentsBlock[$(this).attr("block-hour")] = $(this).val();
+        localStorage.setItem("appointmentsBlock", JSON.stringify(appointmentsBlock));
+
+        // console.log(localStorage);
+        // console.log(appointmentsBlock);
     });
 
-    localStorage.setItem("appointmentsBlock", JSON.stringify(appointmentsBlock));
-    // console.log(localStorage);
-    // console.log(appointmentsBlock);
+
 });
-
-
-
 
